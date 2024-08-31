321c691581e496a6019b2cdd62e0ec76edf9628d
VihanAggarwal <vihanaggarwal@gmail.com>
2024-08-31 19:12:54 -0400

    Update MyArrayList.java

--- MyArrayList/MyArrayList.java
+++ MyArrayList/MyArrayList.java
@@ -6,7 +6,6 @@ public class MyArrayList
     private Object [] list;
     private int numElements;       // number of students currently in the
                                     // list
-
     // Constructs the list, initially empty,
     // but can hold up to 100
     public MyArrayList ()
@@ -14,7 +13,6 @@ public class MyArrayList
         list = new Object[100];
         numElements = 0;
     }
-
 
     // Adds t to the end of the list
     public void add (Object t)
@@ -38,13 +36,11 @@ public class MyArrayList
         numElements++;
     }
 
-
     // Returns the number of active elements on the list
     public int size ()
     {
         return numElements;
     }
-
 
     // Returns the student in the i'th location (counting from 0)
     // Precondition: 0 <= i < size()
@@ -60,7 +56,6 @@ public class MyArrayList
             throw new IndexOutOfBoundsException();
         }
     }
-
 
     // Sets the i'th location in the list to t;
     // returns the previous i'th element.
@@ -78,7 +73,6 @@ public class MyArrayList
             throw new IndexOutOfBoundsException();
         }
     }
-
     // Removes the i'th element, sliding all items beyond i up by one spot.
     // Returns the element removed
     public Object remove (int i)
